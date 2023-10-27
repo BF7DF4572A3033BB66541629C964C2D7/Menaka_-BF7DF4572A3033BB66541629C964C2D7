96a57cb9d4fcfe6b494d436179b9ea92d058b150
BF7DF4572A3033BB66541629C964C2D7 <149155685+BF7DF4572A3033BB66541629C964C2D7@users.noreply.github.com>
2023-10-27 15:09:11 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,12 @@
+#1.1implement a recursive funtion to callculate the factroial of a number
+def fact_rec(n):
+  
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+
+number=2
+result=fact_rec (number)
+             
+print("The factorial of {}is{}".format(number,result))
